ae395a2b0484b612b57529422f09ff79f739e05e
nicnac29 <nicnac29@gmail.com>
2015-01-22 18:30:28 -0800

    Update ChatBot.java

--- src/com/nic/ChatBot.java
+++ src/com/nic/ChatBot.java
@@ -2,5 +2,34 @@ package com.nic;
 
 public class ChatBot
 {
+private String userResponse;
 
+	public static void main(String[] args) throws IOException
+	{
+		ChatBot frank = new ChatBot();
+		frank.letsChat();
+	}
+
+	public void letsChat() throws IOException
+	{
+
+		do
+		{
+			listenForSomeoneToSaySomethingToMe();
+			saySomethingBack();
+		} while (true);
+	}
+
+	private void listenForSomeoneToSaySomethingToMe() throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Say something to the chatbot:");
+		userResponse = br.readLine();
+	}
+
+	private void saySomethingBack()
+	{
+		System.out.println("sup");
+		System.out.println("you just said: " + userResponse);
+	}
 }
